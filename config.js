79929ef109fdc41f233e0d8df0d1bd5cdcac6c71
Joy Lee <thejoylee@gmail.com>
2020-04-20 22:34:45 +0800

    update

--- config.js
+++ config.js
@@ -1,72 +1,51 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA';
-
-var chapters = {
-    'part-1': {
-        center: [114.208237, 22.290008],
-        zoom: 3.5,
-        bearing: 0,
-        pitch: 0,
-    },
-    'part-2': {
-        center: [77.1016, 28.6375],
-        zoom: 10,
-        bearing: 0,
-        pitch: 0,
-    },
-    'part-3': {
-        center: [72.8561, 19.0022],
-        zoom: 11.1,
-        bearing: 0,
-        pitch: 52,
-    },
-    'part-4': {
-        center: [82.8, 23.88],
-        zoom: 4,
-        bearing: 0,
-        pitch: 0,
-    }
-};
-
-var map;
-
-window.onload = function() {
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/thejoylee/ck92ltjd029zs1ik4li5q8qbj',
-        center: [114.208237, 22.290008],
-        zoom: 4,
-        bearing: 0,
-        pitch: 0,
-    });
-}
-
-// On every scroll event, check which element is on screen
-window.onscroll = function() {
-    var chapterNames = Object.keys(chapters);
-    console.log(chapterNames);
-    for (var i = 0; i < chapterNames.length; i++) {
-        var chapterName = chapterNames[i];
-        if (isElementOnScreen(chapterName)) {
-            setActiveChapter(chapterName);
-            break;
+var config = {
+    style: 'mapbox://styles/thejoylee/ck92ltjd029zs1ik4li5q8qbj',
+    accessToken: 'pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA',
+    showMarkers: true,
+    theme: 'light',
+    alignment: 'left',
+    title: 'Hong Kong History: A Century of Cinema, Culture, and Politics from 1900s-2000s',
+    subtitle: 'Discovering the history of Hong Kong through the growth and experiences of cinema.',
+    byline: 'By Joy Lee',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Pre-1900s',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [114.208237, 22.290008],
+                zoom: 6,
+                pitch: 60,
+                bearing: 0
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'other-identifier',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 13.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
         }
-    }
+    ]
 };
-
-var activeChapterName = 'part-1';
-function setActiveChapter(chapterName) {
-    if (chapterName === activeChapterName) return;
-
-    map.flyTo(chapters[chapterName]);
-
-    document.getElementById(chapterName).setAttribute('class', 'active');
-    document.getElementById(activeChapterName).setAttribute('class', '');
-
-    activeChapterName = chapterName;
-}
-
-function isElementOnScreen(id) {
-    var element = document.getElementById(id);
-    var bounds = element.getBoundingClientRect();
-    return bounds.top < window.innerHeight && bounds.bottom > 0;
-}
